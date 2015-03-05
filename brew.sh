1570099abc0b3a3d9becb49c31eb4c40d07f6f95
Daniel <Daniel.Scholl@verizon.com>
2015-03-05 12:50:45 -0600

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -94,5 +94,10 @@ brew install node
 # Install mackup
 brew install mackup
 
+# Install DevOps
+brew install ansible
+brew install docker
+brew install boot2docker
+
 # Remove outdated versions from the cellar.
 brew cleanup
