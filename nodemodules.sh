552c4a07318767718194e6ea05c9cc442f265907
Daniel <Daniel.Scholl@verizon.com>
2016-11-29 14:55:24 -0600

    Update nodemodules.sh

--- nodemodules.sh
+++ nodemodules.sh
@@ -1,13 +1,14 @@
+npm install -g npm@latest
 npm install -g yarn
-npm install -g yo
-npm install -g grunt-cli
-npm install -g bower
-npm install -g gulp
-npm install -g less
-npm install -g mocha
-npm install -g node-static
-npm install -g nodemon
-npm install -g azure-cli
-npm install -g protractor
-npm install -g karma-cli
-npm install -g avn avn-nvm avn-n
+yarn global add yo
+yarn global add grunt-cli
+yarn global add bower
+yarn global add gulp
+yarn global add less
+yarn global add mocha
+yarn global add node-static
+yarn global add nodemon
+yarn global add azure-cli
+yarn global add protractor
+yarn global add karma-cli
+yarn global add avn avn-nvm avn-n
