edac7a0edfd22d267b1e80bb2ba7eab04470f48f
Daniel <Daniel.Scholl@verizon.com>
2016-12-15 10:25:15 -0600

    Update brewcask.sh

--- brewcask.sh
+++ brewcask.sh
@@ -13,7 +13,7 @@ brew tap caskroom/versions
 brew cask install sublime-text3
 brew cask install visual-studio-code
 brew cask install beyond-compare
-brew cask install iterm2
+#brew cask install iterm2
 brew cask install vagrant
 brew cask install virtualbox
  
