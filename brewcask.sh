1d1efe246728d84f5381fc2ad9e1681941a243b3
Daniel <Daniel.Scholl@verizon.com>
2016-12-15 09:49:55 -0600

    Update brewcask.sh

--- brewcask.sh
+++ brewcask.sh
@@ -6,18 +6,19 @@ brew install brew-cask
 brew tap caskroom/versions
  
 # browser
-brew cask install firefox
-brew cask install google-chrome
+#brew cask install firefox
+#brew cask install google-chrome
  
 # development
 brew cask install sublime-text3
+brew cask install visual-studio-code
 brew cask install beyond-compare
 brew cask install iterm2
 brew cask install vagrant
 brew cask install virtualbox
  
 # other
-brew cask install dropbox
-brew cask install macdown
+#brew cask install dropbox
+#brew cask install macdown
 brew cask install slack
 brew cask install caffeine
